a4e91b37f05a5416e0da393265a975204798fa83
Michael J Peppers <mcgdevfunk824@protonmail.com>
2018-09-13 19:28:26 -0700

    Create app_menu_template.js

---
+++ src/menu/app_menu_template.js
@@ -0,0 +1,33 @@
+import { app } from 'electron';
+import { aboutMenuItem } from './items/about';
+import { checkForUpdatesMenuItem } from './items/check_for_updates';
+import { settingsMenu } from './settings_menu_template';
+
+// This is the "Application" menu, which is only used on macOS
+export const appMenuTemplate = {
+    label: 'Android Messages',
+    submenu: [,
+        aboutMenuItem,
+        checkForUpdatesMenuItem,
+        {
+            type: 'separator'
+        },
+        settingsMenu,
+        {
+            type: 'separator'
+        },        
+        {
+            label: 'Hide D.A.M.',
+            accelerator: 'Command+H',
+            click: () => app.hide()
+        },
+        {
+            type: 'separator',
+        },
+        {
+            label: 'Quit',
+            accelerator: 'Command+Q',
+            click: () => app.quit(),
+        }
+    ]
+};
